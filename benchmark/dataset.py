7d9346d6e89e7cc9be25779c50fc0665d1596c6f
Pongpanod Sankosik <pongpanod.san@cjexpress.co.th>
2022-12-15 17:30:10 +0700

    feat: Implement load_titnaic dataset for DatasetLoader

--- benchmark/dataset.py
+++ benchmark/dataset.py
@@ -35,10 +35,26 @@ class DatasetLoader(object):
         dataset_url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
         dataset = pd.read_csv(dataset_url)
 
+        dataset = pd.concat([
+            dataset,
+            pd.get_dummies(dataset.Sex, prefix='Sex'), 
+            pd.get_dummies(dataset.Embarked, prefix='Embarked')
+            ])
+        dataset = dataset.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1)
+
+        # TODO: Deal with missing value
         train = dataset.iloc[:-100, 1:]
         test = dataset.iloc[-100:, 1:]
-        
-        x_train, y_train = train.iloc[:, 0].values, train.iloc[:, 1:].values
-        x_test, y_test = test.iloc[:, 0].values, test.iloc[:, 1:].values
 
+        x_train, y_train = train.iloc[:, 1:].values, train.iloc[:, 0].values
+        x_test, y_test = test.iloc[:, 1:].values, test.iloc[:, 0].values
 
+        # normalize
+        scaler = StandardScaler()
+        label_encoder = LabelEncoder()
+        x_train = scaler.fit_transform(x_train)
+        y_train = label_encoder.fit_transform(y_train)
+        x_test = scaler.transform(x_test)
+        y_test = label_encoder.transform(y_test)
+    
+        return x_train, y_train, x_test, y_test
